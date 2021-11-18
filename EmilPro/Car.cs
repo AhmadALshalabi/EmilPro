39f8bdded8608e96595009f63aca044fecad4df5
AhmadALshalabi <78736540+AhmadALshalabi@users.noreply.github.com>
2021-11-18 10:37:00 +0100

    Changing in car class

---
+++ EmilPro/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmilPro
+{
+    internal class Car
+    {
+        public void Create()
+        {
+            Console.WriteLine("its a car1 ");
+        }
+    }
+}
